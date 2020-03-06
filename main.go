bc3cfef52d0934192f82ec35fbba233029366c47
Ethan Godt <ethan@ethangodt.com>
2020-03-05 16:33:42 -0800

    step 1

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package shellby
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// what is os.Stdin
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		// why is it printing delim
+		// why do I have to hit enter?
+		fmt.Print("s: ")
+		text, err := reader.ReadString('\n')
+		if err == io.EOF {
+			fmt.Print("\nSee ya!\n")
+			return
+		}
+		fmt.Print(text, "blah", err)
+	}
+}
